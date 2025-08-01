648034f5c09e7453d1eb930b5db004cf65abee9f
durga prasad <kingprasad4426@gmail.com>
2025-08-01 10:03:07 +0530

    changes added

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:sac_app/available_crps.dart';
 // import 'package:sac_app/advise.dart';
-// import 'package:sac_app/market.dart';  
+// import 'package:sac_app/market.dart';
 
 void main() {
   if (kIsWeb) {
@@ -18,10 +18,11 @@ class SacApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, 
-    // home: MarketPage(),
-    // home: AdvisePage(),
-    home: AvailableCrps(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: MarketPage(),
+      // home: AdvisePage(),
+      home: AvailableCrps(),
     );
   }
 }
